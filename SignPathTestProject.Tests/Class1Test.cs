76173c84e0963ca97eade0516ec5b2cfb57bdf4c
Dominik Rauch <dominik@rauch.io>
2019-07-03 08:41:55 +0200

    Add sample solution so we can build something in the project.

---
+++ SignPathTestProject.Tests/Class1Test.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace SignPathTestProject.Tests
+{
+  [TestFixture]
+  public class Class1Test
+  {
+    [Test]
+    public void Foo()
+    {
+      var sut = new Class1();
+
+      // ACT
+      var result = sut.Foo();
+
+      // ASSERT
+      result.Should().Be("Bar");
+    }
+  }
+}
---
+++ SignPathTestProject/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace SignPathTestProject
+{
+  /// <summary>
+  ///   Sample class
+  /// </summary>
+  public class Class1
+  {
+    /// <summary>
+    ///   Sample method
+    /// </summary>
+    public string Foo() => "Bar";
+  }
+}
